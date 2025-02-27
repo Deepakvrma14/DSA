5862ebed7d90c5bcd0c3d84aadde991e85d131b6
Deepak Verma <86903293+Deepakvrma14@users.noreply.github.com>
2025-02-27 14:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (41.11%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,22 +1,23 @@
 func twoSum(nums []int, target int) []int {
-    for i:= 0 ; i< len(nums) ; i++ {
-        for j:= i+1 ; j<len(nums) ; j++ {
-            if (nums[i] + nums[j] == target){
-                return []int{i,j}
-            }
-        }
-    }
-    return nil
-
-    // numToIndexMap := make(map[int]int)
-
-    // for i, num := range nums {
-    //     diff := target - num
-    //     if idx, found := numToIndexMap[diff]; found {
-    //         return []int{i, idx}
+    // for i:= 0 ; i< len(nums) ; i++ {
+    //     for j:= i+1 ; j<len(nums) ; j++ {
+    //         if (nums[i] + nums[j] == target){
+    //             return []int{i,j}
+    //         }
     //     }
-    //     numToIndexMap[num] = i
     // }
-
     // return nil
+
+   indexMap := make(map[int]int)
+
+   for i:=0;i<len(nums);i++{
+        
+        diff:= target-nums[i]
+
+        if idx,ok :=indexMap[(diff)]; ok{
+            return []int{i,idx}
+        }
+        indexMap[(nums[i])] = i;
+   }
+   return nil;
 }
