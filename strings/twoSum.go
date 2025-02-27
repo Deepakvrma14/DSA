4fd850dae02411e997b1a55f5635ef4252b21f3d
Deepak <deepak.v@wijungle.com>
2025-02-27 15:19:22 +0530

    twoSum

--- strings/twoSum.go
+++ strings/twoSum.go
@@ -1,15 +1,43 @@
 func twoSum(nums []int, target int) []int {
-	numToIndexMap := make(map[int]int)
+	// for i:= 0 ; i< len(nums) ; i++ {
+	//     for j:= i+1 ; j<len(nums) ; j++ {
+	//         if (nums[i] + nums[j] == target){
+	//             return []int{i,j}
+	//         }
+	//     }
+	// }
+	// return nil
 
-	for i, num := range nums {
-		diff := target - num
+	indexMap := make(map[int]int)
 
-		if idx, found := numToIndexMap[diff]; found {
+	for i := 0; i < len(nums); i++ {
+
+		diff := target - nums[i]
+
+		if idx, ok := indexMap[(diff)]; ok {
 			return []int{i, idx}
 		}
-
-		numToIndexMap[num] = i
+		indexMap[(nums[i])] = i
 	}
-
 	return nil
+
+	// greedy approach but not space efficienet just to tell if solution possible or not only
+	// numsCopy := make([]int, len(nums))
+	// copy(nums, numsCopy)
+	// sort.Ints(numsCopy)
+	// l:=0
+	// r:=len(numsCopy) -1
+
+	// for l < r {
+	//     if(numsCopy[l] + numsCopy[r] ==target){
+	//         break
+	//     }else if (numsCopy[l]+numsCopy[r] <target){
+	//         l++
+	//     }else{
+	//         r--
+	//     }
+
+	// }
+	// return nil
+
 }
